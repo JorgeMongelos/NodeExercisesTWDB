ceafd51c4c5a9626d1c0d02604a51f11971dcece
JorgeMongelos <jorge_mongelos_wi@outlook.com>
2020-05-17 04:54:55 -0500

    Initial Commit

---
+++ 7-YelpCamp/app.js
@@ -0,0 +1,45 @@
+/**********************
+    Export libraries
+**********************/ 
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var request = require("request");
+
+/*****************************************************
+    set express to look for .ejs files automatically
+*****************************************************/
+app.set("view engine", "ejs");
+
+/****************************************************
+    init body-parser to parse string variables into
+    .js variables
+****************************************************/
+app.use(bodyParser.urlencoded({extended: true}));
+
+/*********** 
+    routes
+***********/
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    var campGrounds = [
+        {name:"Tiger Musky", image:"http://tigermuskyresort.com/wp-content/gallery/tigermusky/8tiger-musky-cabin.jpg" },
+        {name:"Shady Nook", image:"https://img1.wsimg.com/isteam/ip/2974abf5-b960-408d-a068-84d1b44e6734/acf70465-07bc-463f-80ad-5731fd2f54e3.JPG/:/rs=w:400,h:500,cg:true,m/cr=w:800,h:500,a:cc" },
+        {name:"Sisko's Pine Point", image:"https://siskosresort.com/wp-content/uploads/2019/11/eaglesfrommark.jpg" }
+    ];
+
+    res.render("campgrounds", {campGrounds: campGrounds});
+});
+
+
+
+
+/********************************
+    init port to listen for app
+********************************/
+app.listen(3000, function(){
+    console.log("Server listening on port 3000");
+});
